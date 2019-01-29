8b6848c595f30c6938bc2f85695ba9db64df5019
Sean <Spglancy@gmail.com>
2019-01-28 17:15:45 -0800

    is broke

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
 import inventory, { categories } from './inventory'
+import Button from './Button'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      active: '',
+    }
+  }
+
+  getCategories() {
+      return categories.map((cat) => {
+        return <Button cat={cat} onClick={() => this.handleClick(cat)} />
+      })
+  }
+
+  handleClick(cat) {
+    this.setState({active: cat})
+    console.log(cat)
+    console.log(this.state.active)
+  }
+
+  getInventory() {
+    const items = inventory.filter((item) => {
+      return item.category = this.state.active
+    })
+    return items.map(({ id, name, price }) => {
+      return(
+        <div key={id}>
+          <h1>{name}</h1>
+          <p>{price}</p>
+        </div>
+      )
+    })
+  }
 
   render() {
     return (
@@ -10,12 +44,12 @@ class App extends Component {
         <h1>Show products here</h1>
 
         <ul>
-          {/* List product categories here */}
+          {this.getCategories()}
         </ul>
 
-        <ul>
-          {/* Products listed here */}
-        </ul>
+        <div>
+          {this.getInventory()}
+        </div>
 
       </div>
     );
